47c6e452f761255521930e5fa8984feb08326cb8
Stefan Davis <stefan@turnkeylinux.org>
2017-01-22 23:12:23 +0000

    Added basic proxy validation

--- plugins.d/Proxy_Settings/apt.py
+++ plugins.d/Proxy_Settings/apt.py
@@ -1,6 +1,7 @@
 ''' Set APT's HTTP Proxy '''
 from re import match, sub, MULTILINE, search
 from os.path import isfile
+from urlparse import urlparse
 
 CONF = '/etc/apt/apt.conf.d/80proxy'
 PROXY_LINE = r'Acquire::http::Proxy "(.*)";'
@@ -32,12 +33,27 @@ def set_proxy(prox):
     with open(CONF, 'wb') as fob:
         fob.write(data)
     
-                
+def validate_address(addr):
+    parsed = urlparse(addr)
+    return parsed.scheme and len(parsed.netloc.split('.')) > 1
 
 def doOnce():
     pass
 
 def run():
-    prox = console.inputbox('Set proxy', 'Set a HTTP Proxy', init = get_proxy())
-    if prox[0] == 0:
-        set_proxy(prox[1])
+    original_proxy = get_proxy()
+    while True:
+        prox = console.inputbox('Set proxy', 'Set a HTTP Proxy. Must contain scheme "http://example.com" but not "example.com"', init = original_proxy)
+        if prox[0] == 0:
+            if prox[1] and not validate_address(prox[1]):
+                console.msgbox('Invalid Proxy', 'A valid proxy address must atleast have a net location and scheme (http://example.com) but not (example.com)')
+            else:
+                if not prox[1] and original_proxy:
+                    # if no proxy was chosen but there WAS a proxy set previously
+                    if console.yesno('Are you sure you want to disable apt proxy?') == 0:
+                        set_proxy(prox[1])
+                else:
+                    set_proxy(prox[1])
+                break
+        else:
+            break
