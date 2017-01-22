d8d2ad8f4a5f0964861d2917752e8ad0f8eebf39
Stefan Davis <stefan@turnkeylinux.org>
2017-01-22 23:18:10 +0000

    Added ability to cancel hostname setting

--- plugins.d/System_Settings/hostname.py
+++ plugins.d/System_Settings/hostname.py
@@ -6,5 +6,6 @@ TITLE = 'Update Hostname'
 
 def run():
     ret, new_hostname = console.inputbox(TITLE, 'Please enter the new hostname for this machine:')
-    os.system("hostname %s && echo '%s' > /etc/hostname" % (new_hostname, new_hostname))
-    console.msgbox(TITLE, 'Hostname updated successfully. Some applications might require a relaunch before the setting applies to them.')
+    if ret == 0:
+        os.system("hostname %s && echo '%s' > /etc/hostname" % (new_hostname, new_hostname))
+        console.msgbox(TITLE, 'Hostname updated successfully. Some applications might require a relaunch before the setting applies to them.')
