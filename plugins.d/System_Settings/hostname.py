c9897a4d54b1f35cde96e524fa5c42c17217c8cb
Stefan Davis <stefan@turnkeylinux.org>
2017-02-01 21:06:26 +0000

    Fixed hostname verification

--- plugins.d/System_Settings/hostname.py
+++ plugins.d/System_Settings/hostname.py
@@ -1,11 +1,26 @@
 '''Update machine hostname'''
 
 import os
+from subprocess import Popen, CalledProcessError, PIPE
 
 TITLE = 'Update Hostname'
 
 def run():
-    ret, new_hostname = console.inputbox(TITLE, 'Please enter the new hostname for this machine:')
-    if ret == 0:
-        os.system("hostname %s && echo '%s' > /etc/hostname" % (new_hostname, new_hostname))
-        console.msgbox(TITLE, 'Hostname updated successfully. Some applications might require a relaunch before the setting applies to them.')
+    while True:
+        ret, new_hostname = console.inputbox(TITLE, 'Please enter the new hostname for this machine:')
+        if ret == 0:
+            proc = Popen(["hostname", new_hostname], stderr=PIPE)
+            _, out = proc.communicate()
+            returncode = proc.returncode
+
+            if returncode:
+                console.msgbox(TITLE, '{} ({})'.format(out, new_hostname))
+                continue
+
+            with open('/etc/hostname', 'w') as fob:
+                fob.write(new_hostname + '\n')
+            console.msgbox(TITLE, 'Hostname updated successfully. Some applications might require a relaunch before the setting applies to them.')
+
+            break
+        else:
+            break
