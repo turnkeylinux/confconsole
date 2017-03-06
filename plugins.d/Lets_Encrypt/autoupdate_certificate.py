4f1acb1280e8ddc63109aad0008e23d35c5fc91f
Jeremy Davis <jeremy@turnkeylinux.org>
2017-03-06 23:34:58 +0000

    add error message if trying to enable cron job without first creating a cert

--- plugins.d/Lets_Encrypt/autoupdate_certificate.py
+++ plugins.d/Lets_Encrypt/autoupdate_certificate.py
@@ -1,8 +1,8 @@
 """Enable/Disable auto-renew cronjob"""
 
-from os import chmod, stat
+from os import chmod, stat, path
 
-CRON_PATH='/etc/cron.weekly/confconsole-dehydrated'
+CRON_PATH='/etc/cron.daily/confconsole-dehydrated'
 
 def enable_cron():
     st = stat(CRON_PATH)
@@ -13,19 +13,26 @@ def disable_cron():
     chmod(CRON_PATH, st.st_mode ^ 0o111)
 
 def check_cron():
-    st = stat(CRON_PATH)
-    return st.st_mode & 0o111 == 0o111
+    if path.isfile(CRON_PATH):
+        st = stat(CRON_PATH)
+        return st.st_mode & 0o111 == 0o111
+    else:
+        return 'fail'
 
 def run():
     enabled = check_cron()
-    status = 'enabled' if enabled else 'disabled'
-    msg = '''Automatic certificate renewal is currently {}''' 
-    r = console._wrapper('yesno', msg.format(status), 10, 30, yes_label='Toggle', no_label='Ok')
-    while r == 0:
-        if enabled:
-            disable_cron()
-        else:
-            enable_cron()
-        enabled = check_cron()
+    if enabled == 'fail':
+        msg = '''Cron job for dehydrated does not exist.\nPlease "Get certificate" first.'''
+        r = console.msgbox('Error', msg)
+    else:
         status = 'enabled' if enabled else 'disabled'
-        r = console._wrapper('yesno', msg.format(status), 10, 30, yes_label='Toggle', no_label='Ok') 
+        msg = '''Automatic certificate renewal is currently {}''' 
+        r = console._wrapper('yesno', msg.format(status), 10, 30, yes_label='Toggle', no_label='Ok')
+        while r == 0:
+            if enabled:
+                disable_cron()
+            else:
+                enable_cron()
+            enabled = check_cron()
+            status = 'enabled' if enabled else 'disabled'
+            r = console._wrapper('yesno', msg.format(status), 10, 30, yes_label='Toggle', no_label='Ok') 
