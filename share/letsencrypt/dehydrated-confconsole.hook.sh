05e5919d974a16db01c90621860df7fe65120ab4
Jeremy Davis <jeremy@turnkeylinux.org>
2017-03-08 08:38:12 +0000

    another refactor to trap potential issues

--- share/letsencrypt/dehydrated-confconsole.hook.sh
+++ share/letsencrypt/dehydrated-confconsole.hook.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This dehydrated hook script is packaged with Confconsole.
+# It is designed to be used in conjunction with the TurnKey dehydrated-wrapper.
+# For more info, please see https://www.turnkeylinux.org/docs/letsencypt
+
 function hook_log {
     echo "INFO: ${1} ($(date "+%Y-%m-%d %H:%M:%S"))"
 }
@@ -26,15 +30,17 @@ function deploy_cert {
     hook_log "writing cert.pem & cert.key for $DOMAIN to /etc/ssl/private"
     hook_log "fullchain: $FULLCHAIN"
     hook_log "keyfile: $KEYFILE"
-    cat "$KEYFILE" > /etc/ssl/private/cert.key
-    cat "$FULLCHAINFILE" > /etc/ssl/private/cert.pem
-    cat "$KEYFILE" >> /etc/ssl/private/cert.pem
+    cat "$KEYFILE" > $TKL_KEYFILE
+    cat "$FULLCHAINFILE" > $TKL_CERTFILE
+    cat "$KEYFILE" >> $TKL_CERTFILE
+    export CERTS_DONE=1
 }
 
 function unchanged_cert {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
 
     hook_log "cert for $DOMAIN is unchanged - nothing to do"
+    export CERTS_DONE=1
 }
 
 HANDLER=$1; shift; $HANDLER $@
