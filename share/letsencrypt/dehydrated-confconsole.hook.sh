014955c40ddb30ba2725f0a670860d567eac98dc
Jeremy Davis <jeremy@turnkeylinux.org>
2017-03-21 04:57:40 +0000

    further improvements to hook logging, plus checks that vars are set

--- share/letsencrypt/dehydrated-confconsole.hook.sh
+++ share/letsencrypt/dehydrated-confconsole.hook.sh
@@ -4,26 +4,32 @@
 # It is designed to be used in conjunction with the TurnKey dehydrated-wrapper.
 # For more info, please see https://www.turnkeylinux.org/docs/letsencypt
 
-function hook_info {
-    echo "INFO: ${1} ($(date "+%Y-%m-%d %H:%M:%S"))"
+function hook_log {
+    default="[$(date "+%Y-%m-%d %H:%M:%S")] $(basename $0):"
+    case ${1} in
+        info)    echo "$default INFO: ${2}";;
+        warning) echo "$default WARNING: ${2}" >&2;;
+        fatal)   echo "$default FATAL: ${2}" >&2; exit 1;;
+    esac
 }
 
-function hook_warning {
-    echo "WARNING: ${1} ($(date "+%Y-%m-%d %H:%M:%S"))" 2>&1
-}
+for var in HTTP HTTP_BIN HTTP_PID HTTP_LOG TKL_KEYFILE TKL_CERTFILE; do
+    eval "z=\$$var"
+    [ -z $z ] && hook_log fatal "$var is not set. Exiting..."
+done
 
 function deploy_challenge {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 
-    hook_info "Deploying challenge for $DOMAIN"
-    hook_info "Serving $WELLKNOWN/$TOKEN_FILENAME on http://$DOMAIN/.well-known/acme-challenge/$TOKEN_FILENAME"
+    hook_log info "Deploying challenge for $DOMAIN"
+    hook_log info "Serving $WELLKNOWN/$TOKEN_FILENAME on http://$DOMAIN/.well-known/acme-challenge/$TOKEN_FILENAME"
     su - -s /bin/bash -c "authbind $HTTP_BIN -d $HTTP_PID -l $HTTP_LOG $WELLKNOWN/$TOKEN_FILENAME"
 }
 
 function clean_challenge {
     local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
 
-    hook_info "Stopping $HTTP daemon"
+    hook_log info "Stopping $HTTP daemon"
     kill -9 $(cat $HTTP_PID)
     rm $HTTP_PID
 }
@@ -31,9 +37,9 @@ function clean_challenge {
 function deploy_cert {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
 
-    hook_warning "writing cert.pem & cert.key for $DOMAIN to /etc/ssl/private"
-    hook_info "fullchain: $FULLCHAIN"
-    hook_info "keyfile: $KEYFILE"
+    hook_log warning "writing cert.pem & cert.key for $DOMAIN to /etc/ssl/private"
+    hook_log info "fullchain: $FULLCHAIN"
+    hook_log info "keyfile: $KEYFILE"
     cat "$KEYFILE" > $TKL_KEYFILE
     cat "$FULLCHAINFILE" > $TKL_CERTFILE
     cat "$KEYFILE" >> $TKL_CERTFILE
@@ -42,7 +48,7 @@ function deploy_cert {
 function unchanged_cert {
     local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
 
-    hook_info "cert for $DOMAIN is unchanged - nothing to do"
+    hook_log info "cert for $DOMAIN is unchanged - nothing to do"
 }
 
 HANDLER=$1; shift; $HANDLER $@
