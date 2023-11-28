fcb076bf24fc167863497aa6a072b6fe29e9dd6e
Jeremy Davis <jeremy@turnkeylinux.org>
2023-11-28 03:42:31 +0000

    fixing all the obvious bugs to the refactored code...

--- plugins.d/Lets_Encrypt/dns_01.py
+++ plugins.d/Lets_Encrypt/dns_01.py
@@ -4,7 +4,8 @@ import subprocess
 import re
 
 from subprocess import PIPE
-from os import path
+from os import makedirs
+from os.path import join, exists, dirname
 from shutil import which
 
 from typing import Optional
@@ -42,7 +43,7 @@ def run_command(command: list[str]) -> tuple[int, str]:
     proc = subprocess.run(command)
     if proc.returncode != 0:
         com = ' '.join(command)
-        return (com.returncode,
+        return (proc.returncode,
                 "Something went wrong when running {com} '{com.stderr}'")
     else:
         return 0, 'success'
@@ -50,15 +51,15 @@ def run_command(command: list[str]) -> tuple[int, str]:
 
 def apt_install(pkgs: list[str]) -> tuple[int, str]:
     """Takes a list of package names, returns tuple(exit_code, message)"""
-    for command in [('apt-get', 'update'),
-                    ('apt-install', *pkgs, '-y')]:
+    for command in [['apt-get', 'update'],
+                    ['apt-install', *pkgs, '-y']]:
         exit_code, string = run_command(command)
         if exit_code != 0:
             return exit_code, string
     return exit_code, string
 
 
-def check_pkg(pkg: str) -> bool:
+def check_pkg(pkg: str) -> tuple[int, str]:
     """Takes a package name and returns True if installed, otherwise False"""
     p = subprocess.run(['apt-cache', 'policy', pkg],
                        capture_output=True, text=True)
@@ -71,6 +72,8 @@ def check_pkg(pkg: str) -> bool:
             else:
                 return (0, "but (incompatible) Debian package detected -"
                            " removing and installing from upstream.")
+    return (1,
+            f"No apt policy results; package {pkg} may not be installable?")
 
 
 def save_config(config: str) -> None:
@@ -83,7 +86,7 @@ def save_config(config: str) -> None:
                 fob.write(line + '\n')
 
 
-def initial_setup() -> tuple[int, str]:
+def initial_setup() -> None:
     '''Check lexicon and deps are installed and ready to go
 
     Returns a tuple of exit code (0 = success) and message
@@ -110,14 +113,14 @@ def initial_setup() -> tuple[int, str]:
         install_venv = True
         remove_lexicon = True
         msg_mid = ("lexicon deb install noted, but we need a newer version"
-                   " removing deb and install via venv."
+                   " removing deb and install via venv.")
     if not msg:
         msg = msg_start + msg_mid + msg_end
     ret = console.yesno(msg, autosize=True)
     if ret != 'ok':
         return
     if remove_lexicon:
-        exit_code, msg = run_command(['apt-get', 'remove', '-y', 'lexicon']
+        exit_code, msg = run_command(['apt-get', 'remove', '-y', 'lexicon'])
         if exit_code != 0:
             console.msgbox(
                 'Error',
@@ -129,22 +132,24 @@ def initial_setup() -> tuple[int, str]:
         if not pip:
             pkgs.append('pip')
         if not python3_venv:
-            pkg.append('python3-venv')
+            pkgs.append('python3-venv')
         if pkgs:
             exit_code, msg = apt_install(pkgs)
             if exit_code != 0:
                 pkgs_l = " ".join(pkgs)
                 console.msgbox(
                     'Error',
-                    f"Apt installing {pkgs_l] failed:\n\n{msg}")
+                    f"Apt installing {pkgs_l} failed:\n\n{msg}")
         pip = which('pip')
         makedirs(dirname(venv), exist_ok=True)
         local_bin = '/usr/local/bin'
-        for command in
-                [('python3', '-m', 'venv', venv),
-                 (pip, 'install', 'dns-lexicon[full]'),
-                 ('ln', '-s', f"{venv}/bin/lexicon", f"{local_bin}/lexicon"),
-                 ('ln', '-s', f"{venv}/bin/tldextract", f"{local_bin}/tldextract")]:
+        for command in [
+                ['python3', '-m', 'venv', venv],
+                [pip, 'install', 'dns-lexicon[full]'],
+                ['ln', '-s', f"{venv}/bin/lexicon", f"{local_bin}/lexicon"],
+                ['ln', '-s', f"{venv}/bin/tldextract", f"{local_bin}/tldextract"]
+                ]:
+            assert isinstance(command, list)
             exit_code, msg = run_command(command)
             if exit_code != 0:
                 com = " ".join(command)
@@ -160,25 +165,11 @@ def initial_setup() -> tuple[int, str]:
                     'Error',
                     "lexicon ({lexicon_bin}) not found where expected ({lexicon_loc})?!")
             else:
-                return
+                return None
         console.msgbox('Error', "Something went wrong... Please report to TurnKey.")
 
 
-def get_providers() -> tuple[Optional[str], Optional[str]]:
-    ret = console.yesno(
-
-        msg_start + msg_mid + msg_end,
-        autosize=True)
-    if ret != 'ok':
-        return (None, 'Please resolve the issue with lexicon to use'
-                      ' dns-01 challenge.')
-
-        apt = subprocess.run(['apt-get', '-y', 'install', 'lexicon'],
-                             encoding=sys.stdin.encoding,
-                             stderr=PIPE)
-        if apt.returncode != 0:
-            return None, apt.stderr.strip()
-
+def get_providers() -> tuple[Optional[list[tuple[str, str]]], Optional[str]]:
     lexicon_bin = which('lexicon')
     if not lexicon_bin:
         return None, 'lexicon is not found on your system, is it installed?'
